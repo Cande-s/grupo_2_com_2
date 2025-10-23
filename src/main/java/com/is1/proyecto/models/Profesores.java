8f575c9c28aebbd64366b5683efaa5bc18788f84
Alan123443 <alanfalcon122@gmail.com>
2025-10-23 19:40:55 -0300

    primero

---
+++ src/main/java/com/is1/proyecto/models/Profesores.java
@@ -0,0 +1,23 @@
+package com.is1.proyecto.models;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("professors")  
+public class Profesores extends Personas{
+    public Integer getLegajo() {
+        return getInteger("legajo"); // Obtiene el valor de la columna 'legajo'
+    }
+
+    public void setDni(Integer legajo) {
+        set("legajo", legajo); // Establece el valor para la columna 'legajo'
+    }
+
+    public String getCargo(){
+        return getString("cargo"); // Obtiene el valor de la columna 'cargo'
+    }
+
+    public void setCargo(String cargo){
+        set("cargo", cargo); // Establece el valor para la columna 'cargo'
+    }
+}
--- target/classes/scheme.sql
+++ target/classes/scheme.sql
@@ -7,3 +7,22 @@ CREATE TABLE users (
     name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
     password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
 );
+
+CREATE TABLE persons (
+    id_per INTEGER,
+    dni INTEGER NOT NULL UNIQUE,
+    nombre varchar(20) NOT NULL,
+    apellido varchar(20) NOT NULL,
+    direccion varchar(50),
+    telefono INTEGER,
+    correo varchar(255) NOT NULL UNIQUE,
+    id_per INTEGER,
+    CONSTRAINT id_fk FOREIGN KEY id_per REFERENCES users(id)
+);
+
+CREATE TABLE professors (
+    id_prof
+    legajo INTEGER,
+    cargo varchar(20),
+    CONSTRAINT id_p_fk FOREIGN KEY id_prof REFERENCES persons(id_per)
+);
